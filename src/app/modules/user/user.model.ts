98702fe0842350d7ae5f885ae51446cf437fa938
rifat-sarker <rifatswd@gmail.com>
2025-05-17 10:27:28 +0600

    added .env.example file

--- src/app/modules/user/user.model.ts
+++ src/app/modules/user/user.model.ts
@@ -1,69 +1,70 @@
-/* eslint-disable @typescript-eslint/no-this-alias */
-import bcrypt from "bcrypt";
-import mongoose, { Schema, model } from "mongoose";
-import { IUser, USER_ROLE, UserModel } from "./user.interface";
-import config from "../../config";
+// /* eslint-disable @typescript-eslint/no-this-alias */
+// import bcrypt from "bcrypt";
+// import mongoose, { Schema, model } from "mongoose";
+// import { IUser, USER_ROLE, UserModel } from "./user.interface";
+// import config from "../../config";
 
-const userSchema = new Schema<IUser>(
-  {
-    name: {
-      type: String,
-      required: true,
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    password: {
-      type: String,
-      required: true,
-      select: 0,
-    },
-    role: {
-      type: String,
-      enum: [USER_ROLE.admin, USER_ROLE.customer],
-      default: USER_ROLE.customer,
-    },
-    phone: { type: String },
-    address: { type: String },
-  },
-  {
-    timestamps: true,
-  }
-);
+// const userSchema = new Schema<IUser>(
+//   {
+//     name: {
+//       type: String,
+//       required: true,
+//     },
+//     email: {
+//       type: String,
+//       required: true,
+//       unique: true,
+//     },
+//     password: {
+//       type: String,
+//       required: true,
+//       select: 0,
+//     },
+//     role: {
+//       type: String,
+//       enum: [USER_ROLE.admin, USER_ROLE.customer],
+//       default: USER_ROLE.customer,
+//     },
+//     phone: { type: String },
+//     address: { type: String },
+//   },
+//   {
+//     timestamps: true,
+//   }
+// );
 
-userSchema.pre("save", async function (next) {
-  // eslint-disable-next-line @typescript-eslint/no-this-alias
-  const user = this; // doc
-  // hashing password and save into DB
+// // userSchema.pre("save", async function (next) {
+// //   // eslint-disable-next-line @typescript-eslint/no-this-alias
+// //   const user = this; // doc
+// //   // hashing password and save into DB
 
-  user.password = await bcrypt.hash(
-    user.password,
-    Number(config.bcrypt_salt_rounds)
-  );
+// //   user.password = await bcrypt.hash(
+// //     user.password,
+// //     Number(config.bcrypt_salt_rounds)
+// //   );
 
-  next();
-});
+// //   next();
+// // });
 
-// set '' after saving password
-userSchema.post("save", function (doc, next) {
-  doc.password = "";
-  next();
-});
+// // // set '' after saving password
+// // userSchema.post("save", function (doc, next) {
+// //   doc.password = "";
+// //   next();
+// // });
 
-userSchema.set("toJSON", {
-  transform: (_doc, ret) => {
-    delete ret.password;
-    return ret;
-  },
-});
+// // userSchema.set("toJSON", {
+// //   transform: (_doc, ret) => {
+// //     delete ret.password;
+// //     return ret;
+// //   },
+// // });
 
-userSchema.statics.isPasswordMatched = async function (
-  plainTextPassword,
-  hashedPassword
-) {
-  return await bcrypt.compare(plainTextPassword, hashedPassword);
-};
-const User = mongoose.model<IUser, UserModel>("User", userSchema);
-export default User;
+// // userSchema.statics.isPasswordMatched = async function (
+// //   plainTextPassword,
+// //   hashedPassword
+// // ) {
+// //   return await bcrypt.compare(plainTextPassword, hashedPassword);
+// // };
+// const Users = mongoose.model<IUser>("User", userSchema);
+
+// export default Users;
--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,30 +1,32 @@
 import { IAdmin } from "../admin/admin.interface";
 import { IUser } from "./user.interface";
-import User from "./user.model"; // Import default export
+// import User from "./user.model";
+
+
 
 const createUserIntoDB = async (userData: IUser) => {
-  const result = await User.create(userData);
-  return result;
+  // const result = await User.create(userData);
+  // return result;
 };
 
 const createadminIntoDB = async (userData: IAdmin) => {
-  const result = await User.create(userData);
-  return result;
+  // const result = await User.create(userData);
+  // return result;
 };
 
 const getAllUsersFromDB = async () => {
-  const result = await User.find();
-  return result;
+  // const result = await User.find();
+  // return result;
 };
 
 const updateUserIntoDB = async (id: string, payload: Partial<IUser>) => {
-  const result = await User.findByIdAndUpdate(id, payload);
-  return result;
+  // const result = await User.findByIdAndUpdate(id, payload);
+  // return result;
 };
 
 const deleteUserFromDB = async (id: string) => {
-  const result = await User.findByIdAndDelete(id);
-  return result;
+  // const result = await User.findByIdAndDelete(id);
+  // return result;
 };
 
 export const UserServices = {
